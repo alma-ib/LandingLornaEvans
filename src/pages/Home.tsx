7a47807be2a132ebfe7c79625406065ef6a8eb36
h3nry <henrycanalla@gmail.com>
2026-05-18 20:41:00 -0300

    fix npn run build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import { Footer } from '../components/Footer/Footer';
 import { GlobalHeader } from '../components/GlobalHeader/GlobalHeader';
 import './Home.css';
